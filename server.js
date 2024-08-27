b9ef05a671920bb2eaebf496a78301dd56d1cccd
Guillaume <guillaumebaillieux88@gmail.com>
2024-08-27 14:32:53 +0200

    server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const Http = require('http');
 
 /* on crée un serveur web qui va écouter sur le port 8888 */
-Http.createServer().listen(8888);
-
+/* on ajoute une fonction anonyme qui va générer une réponse à notre requête */
 Http.createServer(function(request, response){
     response.writeHead(200);
     response.end('Hello World NodeJS !');
