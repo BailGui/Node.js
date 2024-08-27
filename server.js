b1bf8ba6b632aa1918cb1f1e9159ec333319f8e5
Guillaume <guillaumebaillieux88@gmail.com>
2024-08-27 14:31:37 +0200

    server.js

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const Http = require('http');
 
 /* on crée un serveur web qui va écouter sur le port 8888 */
 Http.createServer().listen(8888);
+
+Http.createServer(function(request, response){
+    response.writeHead(200);
+    response.end('Hello World NodeJS !');
+}).listen(8888);
