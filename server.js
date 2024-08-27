8328eb7b8d11e33b7b664a1e9eff2c0110197c94
Guillaume <guillaumebaillieux88@gmail.com>
2024-08-27 14:29:34 +0200

    server.js

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 /* on importe le module standard http qui va gérer ce protocole */
 const Http = require('http');
+
+/* on crée un serveur web qui va écouter sur le port 8888 */
+Http.createServer().listen(8888);
