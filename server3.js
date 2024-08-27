25e5fbd631822005cb074a2ea88ed1978084d980
Guillaume <guillaumebaillieux88@gmail.com>
2024-08-27 15:01:20 +0200

    server3.js

--- server3.js
+++ server3.js
@@ -5,7 +5,7 @@ const Http = require('http');
 /* on ajoute une fonction anonyme qui va générer une réponse à notre requête */
 Http.createServer(function(request, response){
     // création de l'entête qui renvoie notamment le code HTTP
-    // ainsi que le type de contenu 
+    // ainsi que le type de contenu (ici, du HTML)
     response.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
     // texte de la réponse avec un message 
     response.write('<h1>Bonjour, je suis le serveur Node.JS</h1>');
