9a71bdeb86805ca751119bc39a6a9a131d3b549a
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-04 09:52:48 +0200

    IndexJs

--- express1/index.js
+++ express1/index.js
@@ -6,7 +6,13 @@ const app = express();
 
 // définir une route vers la page d'accueil (la racine du site)
 app.get('/', function(requete, response){
+    // définir l'entête HTTP avec son type MIME
     response.setHeader('Content-Type', 'text/plain');
+    // définir le texte à afficher
     response.write("Hello world : Express, vous êtes sur la page d'accueil");
+    // signifier la fin de la réponse pour qu'elle soit envoyée
     response.end();
-})
+});
+
+app.listen(8000);  // le serveur web écoute sur le port 8080
+console.log("Express est démarré et attend votre requête...")
