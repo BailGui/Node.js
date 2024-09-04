d4e3c9e8544cc9089d9c6a8c98fd127b1a1d742f
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-04 11:04:06 +0200

    IndexJsExpress2

--- express2/index.js
+++ express2/index.js
@@ -26,14 +26,23 @@ app.get('/contact', function(requete,response){
 
 // définir une route dynamique
 app.get('/page=:numero', function(requete,response){
+    const monParametre = requete.params.numero;
     // je récupère la valeur de mon paramètre passé dans l'URL
     let numeropage = requete.params.numero;
+
     // définir l'entête HTTP avec son type MIME
     response.setHeader('Content-Type', 'text/plain');
-    // définir le texte à afficher
-    response.write(`Ceci est la page numéro ${numeropage}`);
-    // signifier la fin de la réponse pour qu'elle soit envoyée
-    response.end();
+
+    // je vérifie que la valeur transmise est acceptable
+    // exemple : on suppose qu'on accepte uniquement des valeurs entières de 1 à 5
+    // si oui, j'affiche. Si non, je l'indique par une erreur 
+    if (Number.isInteger(Number(monParametre)) && ((monParametre>0 && monParametre<=5))){
+        response.write("Ceci est la page numéro" + monParametre);
+        // OU BIEN response.write(`Ceci est la page n°${monParametre}`);
+        response.end();
+    } else {
+        response.status(404).send("La valeur de numero n'est pas autorisée !");
+    }
 });
 
 // définir une "page" gérant l'erreur 404 
