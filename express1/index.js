6b665309f4ea47089f327153b07c8dbeaf8fd124
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-04 09:50:53 +0200

    IndexJs

--- express1/index.js
+++ express1/index.js
@@ -4,3 +4,9 @@ const express = require('express');
 // créer un application 
 const app = express();
 
+// définir une route vers la page d'accueil (la racine du site)
+app.get('/', function(requete, response){
+    response.setHeader('Content-Type', 'text/plain');
+    response.write("Hello world : Express, vous êtes sur la page d'accueil");
+    response.end();
+})
