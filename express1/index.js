f379f54485fde93eabd26e8bdf12061cd19fe30e
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-04 10:06:12 +0200

    IndexJs

--- express1/index.js
+++ express1/index.js
@@ -5,13 +5,21 @@ const express = require('express');
 const app = express();
 
 // définir une route vers la page d'accueil (la racine du site)
-app.get('/', function(requete, response){
+app.get('/', function(requete,response){
     // définir l'entête HTTP avec son type MIME
     response.setHeader('Content-Type', 'text/plain');
     // définir le texte à afficher
     response.write("Hello world : Express, vous êtes sur la page d'accueil");
     // signifier la fin de la réponse pour qu'elle soit envoyée
     response.end();
+});
+
+// définir une "page" gérant l'erreur 404 
+app.use(function(requete,response,next){
+    response.setHeader('Content-Type', 'text/plain');
+    response.status(404).send('Page introuvable !');
+    response.end();
+
 });
 
 app.listen(8080);  // le serveur web écoute sur le port 8080
