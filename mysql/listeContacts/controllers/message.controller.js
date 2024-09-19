02389628967f0d48fa190945c640f00bc4b86bee
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-19 16:12:14 +0200

    ModifMessageController

--- mysql/listeContacts/controllers/message.controller.js
+++ mysql/listeContacts/controllers/message.controller.js
@@ -8,11 +8,51 @@ console.log('Je passe dans controllers/message.controller.js');
 
 //Créer et sauver un nouveau message
 exports.create = (req, res) => {
+    console.log('POST /create pour écrire un message');
+    const titrePage = "Formulaire reçu";
+    const lenom = req.body.nom;
+    const lemessage = req.body.msg;
 
+    if ((!req.body) || (lenom == "") || (lemessage == "")) {
+        console.log('Le contenu ne peut pas être vide !');
+        res.redirect('/contact');
+    } else {
+        console.log(req.body);
+
+        const unMsg = new Message({
+            nom: lenom,
+            msg: lemessage
+        });
+
+        Message.create(unMsg, (err, data) => {
+            console.log("message :", unMsg);
+            if (err) {
+                res.status(500).send({
+                    message: "Erreur pendant la création du message"
+                });
+            } else {
+                console.log('Data = ', data);
+                res.render('confirmation', { title: titrePage, nom: unMsg.nom, msg: unMsg.msg });
+            }
+        });
+    }
 };
 
 // Lire tous les messages
 exports.readAll = (req, res) => {
+    console.log('GET /messages/ pour lire tous les messages');
 
+    Message.readAll((err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: 'Erreur pendant la lecture de tous les messages'
+            });
+        } else {
+            console.log('Data : ', data);
+            const titrePage = "Liste de messages";
+            moment.locale('fr'); // pour transformer les dates et heures en français
+            res.render('listeMessages', { title: titrePage, donnees: data, moment: moment });
+        }
+    });
 };
 
--- mysql/listeContacts/routes/messages.js
+++ mysql/listeContacts/routes/messages.js
@@ -6,56 +6,15 @@ const Message = require('../models/message.model.js');
 // import du module 'moment.js"
 var moment = require('moment');
 
+// import du controller message
+const messages = require('../controllers/message.controller.js');
+
 /* Toutes les routers décrites dans ce fichier correspandent à des URL commençant par : localhost:8080/messages/...   */
 
 // route pour l'URL : localhost:8080/messages/
-router.get('/', (req, res) => {
-    console.log('GET /messages/ pour lire tous les messages');
+router.get('/', messages.readAll);
 
-    Message.readAll((err, data) => {
-        if (err) {
-            res.status(500).send({
-                message: 'Erreur pendant la lecture de tous les messages'
-            });
-        } else {
-            console.log('Data : ', data);
-            const titrePage = "Liste de messages";
-            moment.locale('fr'); // pour transformer les dates et heures en français
-            res.render('listeMessages', { title: titrePage, donnees: data, moment: moment });
-        }
-    });
-
-});
-
-router.post('/create', (req, res) => {
-    console.log('POST /create pour écrire un message');
-    const titrePage = "Formulaire reçu";
-    const lenom = req.body.nom;
-    const lemessage = req.body.msg;
-
-    if ((!req.body) || (lenom == "") || (lemessage == "")) {
-        console.log('Le contenu ne peut pas être vide !');
-        res.redirect('/contact');
-    } else {
-        console.log(req.body);
-
-        const unMsg = new Message({
-            nom: lenom,
-            msg: lemessage
-        });
-
-        Message.create(unMsg, (err, data) => {
-            console.log("message :", unMsg);
-            if (err) {
-                res.status(500).send({
-                    message: "Erreur pendant la création du message"
-                });
-            } else {
-                console.log('Data = ', data);
-                res.render('confirmation', { title: titrePage, nom: unMsg.nom, msg: unMsg.msg });
-            }
-        });
-    }
-});
+// route pour l'url create
+router.post('/create', messages.create);
 
 module.exports = router;
