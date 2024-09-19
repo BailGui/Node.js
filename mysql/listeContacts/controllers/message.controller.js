1b3767333aa0b0e82e6a5e08bb80d931e7f17c71
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-19 16:06:10 +0200

    AddMessageController

---
+++ mysql/listeContacts/controllers/message.controller.js
@@ -0,0 +1,18 @@
+// on importe le modèle Message qui contient la connexion
+const Message = require('../models/message.model');
+
+// on importe le module 'moment.js' pour gérer les dates et les heures
+const moment = require('moment');
+
+console.log('Je passe dans controllers/message.controller.js');
+
+//Créer et sauver un nouveau message
+exports.create = (req, res) => {
+
+};
+
+// Lire tous les messages
+exports.readAll = (req, res) => {
+
+};
+
