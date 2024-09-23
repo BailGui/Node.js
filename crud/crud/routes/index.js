9de36180dabe2be759564c6e1d51ee0047a46ba3
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-23 14:05:41 +0200

    majIndexJS

--- crud/crud/routes/index.js
+++ crud/crud/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var root = require("../controllers/root.controller");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', root.home);
+
+/* Afficher le formulaire*/
+router.get('contact', root.form);
+
+/* Afficher les données entrées dans le formulaire*/
+router.post('/traitement', root.traitement);
 
 module.exports = router;
