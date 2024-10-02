b50793f54398f6c09e85613fc86e6a6db929c938
Guillaume <guillaumebaillieux88@gmail.com>
2024-10-02 13:20:51 +0200

    majCrudView

--- crud/crud/models/message.model.js
+++ crud/crud/models/message.model.js
@@ -57,4 +57,79 @@ Message.readById = function (resultat) {
     });
 };
 
+Message.UpdateById = (resultat) => {
+    sql.query("SELECT * FROM messages WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("Erreur lors de la lecture dans Message.UpdateById : " + err);
+            resultat(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("Message.UpdateById - message trouvé : ", res[0]);
+            resultat(null, res[0]);
+            return;
+        }
+
+        resultat({ type: "ERR_NOT_FOUND" }, null);
+    });
+};
+
+Message.update = (resultat) => {
+    sql.query("UPDATE messages SET nom=?, message=? WHERE id=?", [msg.nom, msg.msg, id], (err, res) => {
+        if (err) {
+            console.log("Erreur lors de la lecture dans Message.update : " + err);
+            resultat(err, null);
+            return;
+        }
+
+        if (res.affectedRows == 0) {
+            resultat({ type: "ERR_NOT_FOUND" }, null);
+            return;
+        }
+
+        resultat({ type: "ERR_NOT_FOUND" }, null);
+    });
+};
+
+Message.deleteById = function (id, resultat) {
+    sql.query("SELECT * FROM messages WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("Erreur Message.deleteById : ", err);
+            resultat(err, null);
+            return;
+        }
+        if (res.length) {
+            console.log("Message.deleteById - message trouvé : ", res[0]);
+            resultat(null, res[0]);
+            return;
+        }
+        // Pas de message trouvé avec cet ID
+        resultat({ type: "ERR_NOT_FOUND" }, null);
+    });
+};
+
+// Méthode pour supprimer un message dans la DB
+Message.delete = (id, resultat) => {
+    sql.query("DELETE FROM messages WHERE id = ?", [id], (err, res) => {
+        if (err) {
+            console.log("Erreur Message.delete :", err);
+            resultat(err, null);
+            return;
+        }
+        console.log("RES=" + res);
+
+        if (res.affectedRows == 0) {
+            // Pas de message trouvé avec cet ID
+            resultat({ type: "ERR_NOT_FOUND" }, null);
+            return;
+        }
+        console.log('Message.delete - message' + id + " supprimé");
+        resultat(null, res);
+    });
+};
+
+// Envoyer les nouvelles données après modification 
+
+
 module.exports = Message;
