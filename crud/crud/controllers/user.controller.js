e63960df5e1a3fde1f59a91fbcff42e8d4ebccdf
Guillaume <guillaumebaillieux88@gmail.com>
2024-10-07 15:15:54 +0200

    MajIndexEJS

---
+++ crud/crud/controllers/user.controller.js
@@ -0,0 +1,28 @@
+console.log('On passe dans controllers/user.controller.js');
+
+// On importe le modèle User avec la connexion de la DB
+const User = require("../models/user.model.js");
+// Aller sur la page d'accueil de l'utilisateur connecté
+exports.home = (req, res) => {
+
+};
+// Afficher le formulaire d'enregistrement 
+exports.registerform = (req, res) => {
+
+};
+// Enregister l'utilisateur dans la DB
+exports.register = (req, res) => {
+
+};
+// Afficher le formulaire de connexion 
+exports.loginform = (req, res) => {
+
+};
+// se connecter
+exports.login = (req, res) => {
+
+};
+// se déconnecter
+exports.logout = (req, res) => {
+
+};
